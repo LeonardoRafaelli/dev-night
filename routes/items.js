e4e775da01fd7f542139b32315ef88f9b887c6f6
gabrielforster <rochafrgabriel@gmail.com>
2022-06-16 19:45:22 -0300

    feat: init items method

--- routes/items.js
+++ routes/items.js
@@ -2,14 +2,31 @@ const express = require('express')
 const router = express.Router()
 router.use(express.json())
 
+//static items
 const items = [
-    {name: 'Item 1', price: 12.99},
-    {name: 'Item 2', price: 13.99},
-    {name: 'Item 3', price: 14.99},
+    {itemId: 1,name: 'Item 1', stockQuantity: 10, description: 'This is item 1', image: 'drive.com/item1.jpg'},
+    {itemId: 2, name: 'Item 2', stockQuantity: 20, description: 'This is item 2', image: 'drive.com/items2.jpg'},
+    {itemId: 3, name: 'Item 3', stockQuantity: 30, description: 'This is item 3', image: 'drive.com/item3.jpg'},
 ]
 
 router.get('/', (req, res) => {
     res.json(items)
+})
+
+router.post('/', async(req, res) => {
+    const newItem = {
+        itemId: items.length + 1,
+        name: req.body.name,
+        stockQuantity: req.body.stockQuantity,
+        description: req.body.description,
+        image: req.body.image,
+    }
+    try{
+        items.push(newItem)
+        res.status(202).json(newItem)
+    }catch (err){
+        res.status(500).json({error: err.message})
+    }
 })
 
 module.exports = router
