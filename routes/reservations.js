5809dbfd0caf89546a3b71bbd6043266e964ef91
rebelattogustavo <83778450+rebelattogustavo@users.noreply.github.com>
2022-06-16 20:15:36 -0300

    reservation update.2

--- routes/reservations.js
+++ routes/reservations.js
@@ -4,20 +4,20 @@ const router = express.Router();
 
 const reservationList = [
     {
-        reservation_id: 1,
-        withdrawn_date: '2022-03-11',
-        return_date: '2022-03-12',
-        withdrawn_hour: '8:00',
-        return_hour: '9:00',
-        teacher_id: 1
+        reservationId: 1,
+        withdrawnDate: '2022-03-11',
+        returnDate: '2022-03-12',
+        withdrawnHour: '8:00',
+        returnHour: '9:00',
+        teacherId: 1
     },
     {
-        reservation_id: 2,
-        withdrawn_date: '2022-03-13',
-        return_date: '2022-03-14',
-        withdrawn_hour: '11:00',
-        return_hour: '18:00',
-        teacher_id: 2
+        reservationId: 2,
+        withdrawnDate: '2022-03-13',
+        returnDate: '2022-03-14',
+        withdrawnHour: '11:00',
+        returnHour: '18:00',
+        teacherId: 2
     },
 ];
 
@@ -27,14 +27,14 @@ function getReservationList() {
 
 function addReservation(req) {
     const reservation = req.body;
-    reservation.reservation_id = reservationList.length + 1;
+    reservation.reservationId = reservationList.length + 1;
     reservationList.push(reservation);
     return reservation;
 }
 
 function takeIdReservation(req) {
-    const reservation_id = req.params.reservation_id;
-    return reservationList.find(reservation => reservation.reservation_id == reservation_id);
+    const reservationId = req.params.reservationId;
+    return reservationList.find(reservation => reservation.reservationId == reservationId);
 }
 
 
@@ -46,15 +46,15 @@ router.post('/', (req, res) => {
     res.json(addReservation(req));
 })
 
-router.put('/:reservation_id', (req, res) => {
+router.put('/:reservationId', (req, res) => {
     const reservation = req.body;
-    reservation.reservation_id = req.params.reservation_id;
+    reservation.reservationId = req.params.reservationId;
     reservationList[takeIdReservation(req)] = reservation;
     res.json(reservation);
     
 })
 
-router.delete('/:reservation_id', (req, res) => {
+router.delete('/:reservationId', (req, res) => {
     reservationList.splice(takeIdReservation(req), 1);
     res.json(reservation);
 })
