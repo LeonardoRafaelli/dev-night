db3c0987cf6ad968cca52fd20330c651a6133a63
LeonardoRafaelli <leonardorafaelli@gmail.com>
2022-06-16 19:35:11 -0300

    update

--- routes/items.js
+++ routes/items.js
@@ -2,13 +2,6 @@ const express = require('express')
 const router = express.Router()
 router.use(express.json())
 
-
-const people = [
-    {id: 1, user: "professor1", passwd: "123"},
-    {id: 2, user: "atendente1", passwd: "123"},
-    {id: 3, user: "adm1", passwd: "123"}
-]
-
 const items = [
     {name: 'Item 1', price: 12.99},
     {name: 'Item 2', price: 13.99},
